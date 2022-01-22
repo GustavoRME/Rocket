f498920be6ecc378289ebd78e15e4238ea74b497
Gustavo <gustavomunozespinoza@hotmail.com>
2022-01-22 14:48:45 -0300

    TextMesh Pro; Parachute script; Rocket prefab

--- Assets/_Project/Scripts/Parachute.cs
+++ Assets/_Project/Scripts/Parachute.cs
@@ -4,100 +4,71 @@ using UnityEngine;
 
 public class Parachute : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rb = default;
-    [SerializeField] private MeshRenderer _parachuteRender = default;
-    
-    [Tooltip("Speed decrease/second")] [SerializeField] private float _speed = 5.0f;
-    [SerializeField] private float _rotationSpeed = 5.0f;
-    [SerializeField] private float _maxDrag = 20.0f;
-    
-    [Header("Parachute rotation speed")]
-    [SerializeField] private float _minSpeed = 15f;
-    [SerializeField] private float _maxSpeed = 30f;
-    
-    [Tooltip("Use to rotation slow down when rocket is looking down")] 
-    [SerializeField] private float _decreaseSpeedRotation = 5.0f;
+    [SerializeField] private Rigidbody _rocketRigidbody = default;
+    [SerializeField] private Rigidbody _parachuteRigidbody = default;
 
-    [Header("Swing simulation")]
-    [SerializeField] private Vector3 _diving00 = Vector3.zero;
-    [SerializeField] private Vector3 _diving01 = Vector3.zero;
-
-    private readonly Vector3[] _divings = new Vector3[2];
-    [SerializeField] private int _index;
+    [Space]
+    [SerializeField] private Transform _noseCone = default;
+    [SerializeField] private Transform _parentRocket = default;
     
-    [SerializeField] private bool _isTurnDown;               //Its true when the Nose Cone is looking for floor
-    [SerializeField] private float _turnSpeed;               //Use as speed rotation when Nose Cone is already looking down. Simulate a pendulum move
+    [Header("Speed and Limits")]
+    [Tooltip("Slow down fall speed/s")]     [SerializeField] private float _slowDownSpeed = 5.0f;
+    [Tooltip("Slow down rotation speed/s")] [SerializeField] private float _slowDownAngular = 10.0f;
+    [SerializeField] private float _maxLinearDrag = 20.0f;
+    [SerializeField] private float _maxAngularDrag = 8.0f;
+   
+    private float _startDrag;
+    private float _startAngularDrag;
 
     private void Awake()
     {
-        _divings[0] = _diving00;
-        _divings[1] = _diving01;
+        _startDrag = _rocketRigidbody.drag;
+        _startAngularDrag = _parachuteRigidbody.angularDrag;
         enabled = false;
     }
 
     private void Update()
     {
         //Slow down fall speed smoothly
-        if(_rb.drag < _maxDrag)
+        if(_rocketRigidbody.drag < _maxLinearDrag)
         {
-            _rb.drag += _speed * Time.deltaTime;
-        }
-#if UNITY_EDITOR
-        else
-        {
-            _rb.drag = _maxDrag;
-        }
-#endif
-
-        //Check if rocket is already complete the rotation
-        if (transform.eulerAngles.z >= 150f && !_isTurnDown)
-            _isTurnDown = true;
-
-        if(transform.eulerAngles == _diving00)
-        {
-            _index = 1;
-        }
-        else if(transform.eulerAngles == _diving01)
-        {
-            _index = 0;
+            _rocketRigidbody.drag += _slowDownSpeed * Time.deltaTime;
         }
 
-        //Rotation has three different speed. The '_rotationSpeed' is used while rocket isn't turned down
-        //The range of speed, '_minSpeed' and '_maxSpeed', is used to simulate the swing while is falling.
-        if (_isTurnDown)
+        //Slow down rotation speed smoothly
+        if(_parachuteRigidbody.angularDrag < _maxAngularDrag)
         {
-            _turnSpeed = IsOnRange(_turnSpeed) ? 
-                GetRandomSpeed() 
-                : _turnSpeed - _decreaseSpeedRotation * Time.deltaTime;
-        }
-        else
-        {
-            _turnSpeed = _rotationSpeed;
+            _parachuteRigidbody.angularDrag += _slowDownAngular * Time.deltaTime;
         }
 
-        transform.eulerAngles = Vector3.MoveTowards(transform.eulerAngles, _divings[_index], _turnSpeed * Time.deltaTime);
+//#if UNITY_EDITOR
+//        if (_rocketRigidbody.drag >= _maxLinearDrag)
+//            _parachuteRigidbody.drag = _maxLinearDrag;
+        
+//        if (_rocketRigidbody.angularDrag >= _maxAngularDrag)
+//            _parachuteRigidbody.angularDrag = _maxAngularDrag;
+//#endif
     }
 
     public void OpenParachute()
     {
-        _rb.angularDrag = 0.0f;
-        _parachuteRender.enabled = true;
+        if (enabled)
+            return;
+
+        _noseCone.SetParent(_parachuteRigidbody.transform);
+        _parachuteRigidbody.gameObject.SetActive(true);
         enabled = true;
     }
 
     public void CloseParachute()
     {
-        _parachuteRender.enabled = false;
+        if (!enabled)
+            return;
+
         enabled = false;
-    }
-
-    private float GetRandomSpeed()
-    {
-        return Random.Range(_minSpeed, _maxSpeed);
-    }
-
-    private bool IsOnRange(float speed)
-    {
-        return speed >= _minSpeed && speed <= _maxSpeed;
+        _rocketRigidbody.drag = _startDrag;
+        _rocketRigidbody.angularDrag = _startAngularDrag;
+        _noseCone.SetParent(_parentRocket);
+        _parachuteRigidbody.gameObject.SetActive(false);
     }
 }
